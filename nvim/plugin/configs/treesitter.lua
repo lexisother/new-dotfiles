f3694569655d467afe41b9477b99cfd93d07ad2d
Alyxia Sother <lexisoth2005@gmail.com>
2021-12-17 19:34:21 +0100

    [nvim] Configure treesitter

---
+++ nvim/plugin/configs/treesitter.lua
@@ -0,0 +1,12 @@
+local treesitter = require("nvim-treesitter.configs")
+
+treesitter.setup {
+	-- Treesitter config
+	ensure_installed = "maintained",
+
+	-- Module config
+	highlight = {
+		enable = true
+	}
+}
+
