f70bb6d32195df501e73c0f28ad13f771a11ee77
Alyxia Sother <lexisoth2005@gmail.com>
2021-12-24 17:14:00 +0100

    [nvim] Add a simple indentline implementation

--- nvim/plugin/interface.lua
+++ nvim/plugin/interface.lua
@@ -1,2 +1,27 @@
-vim.o.number = true
-vim.o.cursorline = true
+-- Indent lines {{{
+
+-- Set up the colors
+vim.cmd("highlight IndentLineColor guifg=red")
+
+local ilines = vim.api.nvim_create_namespace("miniguide")
+local function on_win(_, _, bufnr, _)
+  if bufnr ~= vim.api.nvim_get_current_buf() then
+    return false -- FAIL
+  end
+end
+
+local function on_line(_, _, bufnr, row)
+  local indent = vim.fn.indent(row + 1)
+  for i = 1, indent - 1, 2 do
+    vim.api.nvim_buf_set_extmark(bufnr, ilines, row, i - 1, {
+      virt_text = { { "│", "IndentLineColor" } },
+      virt_text_pos = "overlay",
+      ephemeral = true,
+    })
+    if tata then
+      ree = re
+    end
+  end
+end
+vim.api.nvim_set_decoration_provider(ilines, { on_win = on_win, on_line = on_line })
+-- }}}
