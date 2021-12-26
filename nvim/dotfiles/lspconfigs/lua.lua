1f561d6135bf785213941e2f41d9867d47b5b550
Alyxia Sother <lexisoth2005@gmail.com>
2021-12-26 22:46:44 +0100

    [nvim] Use nvim-lsp-installer for LSP setup

--- nvim/dotfiles/lspconfigs/lua.lua
+++ nvim/dotfiles/lspconfigs/lua.lua
@@ -1,26 +1,35 @@
-local lspconfig = require("lspconfig")
+local servers = require("nvim-lsp-installer.servers")
 local utils = require("dotfiles.lsp.utils")
 
 local runtime_path = vim.split(package.path, ";")
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
 
-lspconfig.sumneko_lua.setup({
-  on_attach = utils.on_attach,
-  settings = {
-    Lua = {
-      runtime = {
-        path = runtime_path,
+local server_available, requested_server = servers.get_server("sumneko_lua")
+if server_available then
+  requested_server:on_ready(function()
+    print("Hi")
+    requested_server:setup({
+      on_attach = utils.on_attach,
+      settings = {
+        Lua = {
+          runtime = {
+            path = runtime_path,
+          },
+          diagnostics = {
+            globals = { "vim" },
+          },
+          workspace = {
+            library = vim.api.nvim_get_runtime_file("", true),
+          },
+          telemetry = {
+            enable = false,
+          },
+        },
       },
-      diagnostics = {
-        globals = { "vim" },
-      },
-      workspace = {
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
-      telemetry = {
-        enable = false,
-      },
-    },
-  },
-})
+    })
+  end)
+  if not requested_server:is_installed() then
+    requested_server:install()
+  end
+end
