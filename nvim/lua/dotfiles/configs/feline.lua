2695cacb3e398e667bd4ba121f07faec64d9c97d
Alyxia Sother <lexisoth2005@gmail.com>
2021-12-26 23:27:45 +0100

    [nvim] Fix Feline's crap default config

--- nvim/lua/dotfiles/configs/feline.lua
+++ nvim/lua/dotfiles/configs/feline.lua
@@ -1,3 +1,85 @@
 local feline = require("feline")
+local defaultpreset = require("feline.presets.default")
+
+-- Why is that block colored?
+defaultpreset.active[1][1] = {
+  provider = "▊ ",
+  hl = {
+    fg = "bg",
+  },
+}
+
+-- The default config has a terrible looking UI for the Git stuff. So, off to
+-- fixing it myself, I guess...
+defaultpreset.active[2][1] = {
+  provider = "git_branch",
+  hl = {
+    fg = "white",
+    bg = "oceanblue",
+    style = "bold",
+  },
+  left_sep = {
+    "slant_left_2",
+    { str = " ", hl = { bg = "oceanblue", fg = "NONE" } },
+  },
+  right_sep = {
+    { str = " ", hl = { bg = "oceanblue", fg = "NONE" } },
+    "slant_right_2",
+    " ",
+  },
+}
+
+defaultpreset.active[2][2] = {
+  provider = "git_diff_added",
+  hl = {
+    fg = "green",
+    bg = "bg",
+  },
+  right_sep = {
+    " ",
+    {
+      str = "vertical_bar_thin",
+      hl = {
+        fg = "fg",
+        bg = "bg",
+      },
+    },
+  },
+}
+defaultpreset.active[2][3] = {
+  provider = "git_diff_changed",
+  hl = {
+    fg = "orange",
+    bg = "bg",
+  },
+  right_sep = {
+    " ",
+    {
+      str = "vertical_bar_thin",
+      hl = {
+        fg = "fg",
+        bg = "bg",
+      },
+    },
+  },
+}
+defaultpreset.active[2][4] = {
+  provider = "git_diff_removed",
+  hl = {
+    fg = "red",
+    bg = "bg",
+  },
+
+  right_sep = {
+    " ",
+    {
+      str = "vertical_bar_thin",
+      hl = {
+        fg = "fg",
+        bg = "bg",
+      },
+    },
+  },
+}
 
 feline.setup()
