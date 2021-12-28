6cb882c5e6fec772c101860236079e4f9d1385bd
Alyxia Sother <lexisoth2005@gmail.com>
2021-12-28 15:19:49 +0100

    [nvim] Fix some fatal flaws in LSP installation

--- nvim/dotfiles/lspconfigs/global.lua
+++ nvim/dotfiles/lspconfigs/global.lua
@@ -4,18 +4,33 @@ local lsp_installer = require("nvim-lsp-installer")
 -- file, so the `on_server_ready` callback won't become cluttered with `if
 -- server.name ==` to specify custom opts.
 local lspservers = {
-  ["tsserver"] = 0,
-  ["sumneko_lua"] = 1,
+  ["tsserver"] = {
+    folder = "tsserver",
+    manual = 0,
+  },
+  ["eslint"] = {
+    folder = "vscode-eslint",
+    manual = 0,
+  },
+  ["spectral"] = {
+    folder = "spectral",
+    manual = 0,
+  },
+  ["sumneko_lua"] = {
+    folder = "sumneko_lua",
+    manual = 1,
+  },
 }
 
 -- Automatically install the language servers
 local install_path = vim.fn.stdpath("data") .. "/lsp_servers"
-for serv, _ in pairs(lspservers) do
-  if vim.fn.empty(vim.fn.glob(install_path .. "/" .. serv)) > 0 then
-    lsp_installer.install(serv)
+for servname, servmeta in pairs(lspservers) do
+  if vim.fn.empty(vim.fn.glob(install_path .. "/" .. servmeta.folder)) > 0 then
+    lsp_installer.install(servname)
   else
-    break
+    goto continue
   end
+  ::continue::
 end
 
 -- Allow installation of 5 language servers at once
@@ -27,7 +42,7 @@ lsp_installer.settings({
 lsp_installer.on_server_ready(function(server)
   local opts = {}
 
-  if lspservers[server.name] == 1 then
+  if lspservers[server.name].manual == 1 then
   else
     server:setup(opts)
   end
