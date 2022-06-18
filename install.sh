12cb3e23e49ce4113aa3813446cce935e94b9e87
Alyxia Sother <lexisoth2005@gmail.com>
2022-06-18 15:52:48 +0200

    [codespaces] Add installation script

---
+++ install.sh
@@ -0,0 +1,33 @@
+export DEBIAN_FRONTEND=noninteractive
+export USERNAME=`whoami`
+
+sudo hostname alyxia.dev
+sudo apt-get update
+sudo apt-get -y install --no-install-recommends apt-utils dialog 2>&1
+sudo apt-get install -y \
+  curl \
+  jq \
+  openssh-client \
+  less \
+  wget \
+  unzip \
+  apt-transport-https \
+  lsb-release
+  
+sudo apt-get install -y \
+  fonts-powerline \
+  zsh
+  
+sudo apt-get autoremove -y
+sudo apt-get autoremove -y
+sudo rm -rf /var/lib/apt/lists/*
+
+rm -rf ~/.oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+git clone --recurse-submodules https://github.com/keanuplayz/dotfiles ~/.dotfiles
+
+echo "export DOTFILES_DISABLE_WELCOME = 1" >> ~/.zshrc
+echo "source ~/.dotfiles/zsh/zshrc" >> ~/.zshrc
+pip install colorama psutil distro
+
+chsh -s /usr/bin/zsh $USERNAME
