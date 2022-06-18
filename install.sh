6b0ca3f43d21efb80f627d357aa1dc0c414141b4
Alyxia Sother <alyxia@riseup.net>
2022-06-18 14:08:30 +0000

    [codespaces] Fix another CRITICAL BUG...

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ rm -rf ~/.oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone --recurse-submodules https://github.com/keanuplayz/dotfiles ~/.dotfiles
 
-echo "export DOTFILES_DISABLE_WELCOME = 1" >> ~/.zshrc
+echo "export DOTFILES_DISABLE_WELCOME=1" >> ~/.zshrc
 echo "source ~/.dotfiles/zsh/zshrc" >> ~/.zshrc
 
 sudo chsh -s /usr/bin/zsh $USERNAME
